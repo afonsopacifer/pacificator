2a5f203885ac30f1f8ea062ec0dcd9be4e49d33d
afonsopacifer <afonsopolis@gmail.com>
2015-11-03 23:06:33 -0200

    regular expression rules added

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var program = require('commander');
-var pkg = require('./package.json');
+// var fs = require('fs');
+// var program = require('commander');
+// var pkg = require('./package.json');
 
 // program basic
-program.version(pkg.version);
-program.description('Pacificate your CSS');
+// program.version(pkg.version);
+// program.description('Pacificate your CSS');
 
-// before
-var css = ".class{color:#fff;}";
+// The CSS
+var css = ".clGDGDASS__s{ bacjground-color:url('dwdwd');}.rer{ color:#660066;}";
+console.log("Before:\n"+css);
 
-// after
-var css_2 = ".class {\n\tcolor: #fff;\n}";
+css = css.replace(/\s/g, ""); //Retira todos os espacos em branco e quebras de linha e identacao
+css = css.toLowerCase(); //Transforma todos os caracteres para minusculo
+css = css.replace(/\'/g, "\""); //Altera todas as ' para "
+css = css.replace(/_/g, "-"); //Altera todas os _ para -
+css = css.replace(/\{/g, " \{\n"); //Adiciona um espaco antes do { e uma quebra de linha apos
+css = css.replace(/(.*[\:])/g, "\t$1"); //Adiciona identacao aos textos terminados em :
+css = css.replace(/\:/g, "\: "); //Adiona um espaco apos os :
+css = css.replace(/\;/g, "\;\n"); //Adiciona uma quebra de linha apos o ;
+css = css.replace(/\}/g, "\}\n\n"); //Adiciona duas quebras de linha apos o }
 
-// show
-console.log(css);
-console.log(css_2);
+//Result
+console.log("After:\n"+css);
 
 // program.command( '<name>' );
 
