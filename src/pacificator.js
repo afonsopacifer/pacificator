ab0b68340ce9cac0dbd55a943e8e6d2f0e61b6aa
afonsopacifer <afonsopolis@gmail.com>
2015-12-03 11:18:52 -0200

    module refactor

--- src/pacificator.js
+++ src/pacificator.js
@@ -7,9 +7,9 @@ function pacificator() {
   this.pacificate = function(path) {
     var stylesheet = fs.readFileSync(path, 'utf8');
 
-    stylesheet = pacificateSyntax(stylesheet);
-    stylesheet = stylesheet.replace(/(.*[\:])/g, "\t$1");       // Adds indentation the texts terminated in :
-    stylesheet = stylesheet.replace(/\t(.*[\:].*[\{])/g, "$1"); // Removes the indentation in all texts  started in : and the followed text terminated in whitespace+{ (for pseudo-classes)
+    stylesheet = pacificateSyntax(stylesheet)
+                 .replace(/(.*[\:])/g, "\t$1")        // Adds indentation the texts terminated in :
+                 .replace(/\t(.*[\:].*[\{])/g, "$1"); // Removes the indentation in all texts  started in : and the followed text terminated in whitespace+{ (for pseudo-classes)
 
     return stylesheet;
   };
@@ -18,9 +18,9 @@ function pacificator() {
     var stylesheet = fs.readFileSync(path, 'utf8'),
         tabs = "\t".repeat(amount);
 
-    stylesheet = pacificateSyntax(stylesheet);
-    stylesheet = stylesheet.replace(/(.*[\:])/g, tabs+"$1");     // Adds indentation the texts terminated in :
-    stylesheet = stylesheet.replace(/\t*(.*[\:].*[\{])/g, "$1"); // Removes the indentation in all texts  started in : and the followed text terminated in whitespace+{ (for pseudo-classes)
+    stylesheet = pacificateSyntax(stylesheet)
+                 .replace(/(.*[\:])/g, tabs+"$1")      // Adds indentation the texts terminated in :
+                 .replace(/\t*(.*[\:].*[\{])/g, "$1"); // Removes the indentation in all texts  started in : and the followed text terminated in whitespace+{ (for pseudo-classes)
 
     return stylesheet;
   };
@@ -29,9 +29,9 @@ function pacificator() {
     var stylesheet = fs.readFileSync(path, 'utf8'),
         spaces = " ".repeat(amount);
 
-    stylesheet = pacificateSyntax(stylesheet);
-    stylesheet = stylesheet.replace(/(.*[\:])/g, spaces+"$1");  // Adds indentation the texts terminated in :
-    stylesheet = stylesheet.replace(/ *(.*[\:].*[\{])/g, "$1"); // Removes the indentation in all texts  started in : and the followed text terminated in whitespace+{ (for pseudo-classes)
+    stylesheet = pacificateSyntax(stylesheet)
+                 .replace(/(.*[\:])/g, spaces+"$1")   // Adds indentation the texts terminated in :
+                 .replace(/ *(.*[\:].*[\{])/g, "$1"); // Removes the indentation in all texts  started in : and the followed text terminated in whitespace+{ (for pseudo-classes)
 
     return stylesheet;
   };
@@ -39,17 +39,17 @@ function pacificator() {
 }
 
 function pacificateSyntax(stylesheet) {
-  stylesheet = stylesheet.replace(/\s/g, "");                 // Removes all whitespace, indentation and line breaks.
-  stylesheet = stylesheet.toLowerCase();                      // converts all characters to lowercase
-  stylesheet = stylesheet.replace(/\'/g, "\"");               // Change all ' to "
-  stylesheet = stylesheet.replace(/_/g, "-");                 // Change all  _ to -
-  stylesheet = stylesheet.replace(/\{/g, " \{\n");            // Adds a whitespace before the { and add a line break after
-  stylesheet = stylesheet.replace(/\/\*/g, "\/\* ");          // Adds a whitespace the /* (for comments)
-  stylesheet = stylesheet.replace(/\*\//g, " \*\/\n\n");      // Adds a line break after the /* (for comments)
-  stylesheet = stylesheet.replace(/\;/g, "\;\n");             // Adds a line break after the ;
-  stylesheet = stylesheet.replace(/\:/g, "\: ");              // Adds a whitespace after the :
-  stylesheet = stylesheet.replace(/[\:] (.* [\{])/g, "\:$1"); // Removes a whitespace after the : when terminated in whitespace+{ (for pseudo-classes)
-  stylesheet = stylesheet.replace(/\}/g, "\}\n\n");           // Adds two line break after the }
+  stylesheet = stylesheet.replace(/\s/g, "")       // Removes all whitespace, indentation and line breaks.
+               .toLowerCase()                      // converts all characters to lowercase
+               .replace(/\'/g, "\"")               // Change all ' to "
+               .replace(/_/g, "-")                 // Change all  _ to -
+               .replace(/\{/g, " \{\n")            // Adds a whitespace before the { and add a line break after
+               .replace(/\/\*/g, "\/\* ")          // Adds a whitespace the /* (for comments)
+               .replace(/\*\//g, " \*\/\n\n")      // Adds a line break after the /* (for comments)
+               .replace(/\;/g, "\;\n")             // Adds a line break after the ;
+               .replace(/\:/g, "\: ")              // Adds a whitespace after the :
+               .replace(/[\:] (.* [\{])/g, "\:$1") // Removes a whitespace after the : when terminated in whitespace+{ (for pseudo-classes)
+               .replace(/\}/g, "\}\n\n");          // Adds two line break after the }
 
   return stylesheet;
 }
